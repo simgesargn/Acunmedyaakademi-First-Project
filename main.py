f428f55e50e49bb8dfc14638a9497e41f079aa66
simgesargn <simge.sargn@gmail.com>
2025-02-21 14:13:21 +0300

    Python ödevi dosyaları eklendi

---
+++ main.py
@@ -0,0 +1,18 @@
+#alias
+from matematik import topla as toplamaIslemi
+from day2 import sayHello
+from human import Human
+#built-in modules
+import random
+from selenium import webdriver
+#package
+
+print(toplamaIslemi(10,20))
+
+print(random.randint(0,100))
+
+human1 = Human("Mirza")
+human1.talk("Merhaba")
+
+chromeDriver = webdriver.Chrome()
+#packages
